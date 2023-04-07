f7941074c1770a4014feb156dc81eb530d55b1f6
Dimitr-Remicio <113401276+Dimitr-Remicio@users.noreply.github.com>
2023-04-07 11:55:20 -0500

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -13,7 +13,7 @@ function createGalleryItem(element) {
         .map(({ preview, original, description }) => {
             return `
             <a class="gallery__item" href="${original}">
-            <img class="gallery__image" src="${preview}" alt="${description}" />
+            <img class="gallery__image" src="${preview}" alt="${description}"/>
             </a>`
         })
         .join('');
